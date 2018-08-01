e38e0e451c1bc264caa85c438e785ef4aa3e8967
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-08-01 17:16:42 -0300

    add propTypes to buildControl

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BuildControl.css';
 
@@ -14,5 +15,12 @@ const buildControl = (props) => (
 			onClick={props.added}>More</button>
 	</div>
 );
+
+buildControl.propTypes = {
+	label: PropTypes.oneOf(['Salad', 'Bacon', 'Cheese', 'Meat']).isRequired,
+	removed: PropTypes.func.isRequired,
+	disabled: PropTypes.bool.isRequired,
+	added: PropTypes.func.isRequired
+};
 
 export default buildControl;
