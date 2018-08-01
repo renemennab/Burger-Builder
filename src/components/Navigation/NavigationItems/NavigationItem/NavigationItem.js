379699a066dbfe5f673416ae79b901c6232267ce
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-08-01 15:39:21 -0300

    add linting to navigationItem

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router-dom';
 import classes from './NavigationItem.css';
 
 const navigationItem = ( props ) => (
-    <li className={classes.NavigationItem}>
-        <NavLink 
-            to={props.link}
-            exact={props.exact}
-            activeClassName={classes.active}>{props.children}</NavLink>
-    </li>
+	<li className={classes.NavigationItem}>
+		<NavLink 
+			to={props.link}
+			exact={props.exact}
+			activeClassName={classes.active}>{props.children}</NavLink>
+	</li>
 );
 
 export default navigationItem;
