6c165aa110c2c30354a3cd898f0596671b38fc1a
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-08-06 16:16:09 -0300

    add linting in axios-orders

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burguer-1d842.firebaseio.com/'
+	baseURL: 'https://react-my-burguer-1d842.firebaseio.com/'
 });
 
 export default instance;
